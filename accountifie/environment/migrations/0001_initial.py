0e959d072132bde9b85378cb89072b023f320c34
imcallister <ian.mcallister@electronifie.com>
2016-02-21 10:06:54 -0500

    re-setting migrations

--- accountifie/environment/migrations/0001_initial.py
+++ accountifie/environment/migrations/0001_initial.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('gl', '0001_initial'),
+        ('gl', '__first__'),
     ]
 
     operations = [
