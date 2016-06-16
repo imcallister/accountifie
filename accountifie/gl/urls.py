be4e2624098c33b4eb85e332e05d067462ee59ee
imcallister <ian.mcallister@electronifie.com>
2016-06-16 15:07:43 -0400

    missing entries

--- accountifie/gl/urls.py
+++ accountifie/gl/urls.py
@@ -5,6 +5,16 @@ import views
 
 urlpatterns = patterns('',
 
+    url(r'^counterparty-autocomplete/$',
+         views.CounterpartyAutocomplete.as_view(),
+         name='counterparty-autocomplete',
+    ),
+ 
+    url(r'^account-autocomplete/$',
+         views.AccountAutocomplete.as_view(),
+         name='account-autocomplete',
+    ),
+
     # admin-type view
     (r'gl/accounts_list/$', views.accounts_list),
     (r'gl/counterparty_list/$', views.counterparty_list),
