9e9f360fefb9f9a9c68dd7d21c2caf1493e7f9e0
imcallister <ian.mcallister@electronifie.com>
2016-06-26 22:35:03 -0400

    adding common migrations

---
+++ accountifie/common/migrations/0001_initial.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Log',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('level', models.CharField(max_length=10)),
+                ('time', models.DateTimeField(auto_now_add=True)),
+                ('message', models.TextField()),
+                ('traceback', models.TextField()),
+                ('request', models.TextField()),
+            ],
+            options={
+                'ordering': ['-time', 'id'],
+                'verbose_name_plural': 'Log entries',
+            },
+        ),
+        migrations.CreateModel(
+            name='TaskMonitor',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('task_id', models.CharField(max_length=200, null=True)),
+                ('creation_timestamp', models.DateTimeField(auto_now_add=True, null=True)),
+                ('task_name', models.CharField(max_length=200, null=True)),
+                ('percent_complete', models.IntegerField(default=0, null=True)),
+                ('task_state', models.CharField(max_length=200, null=True, choices=[(b'started', b'started'), (b'failed', b'failed'), (b'succeeded', b'succeeded')])),
+                ('traceback', models.TextField(null=True)),
+                ('log_info', models.TextField(null=True)),
+                ('initiator', models.CharField(max_length=200, null=True)),
+            ],
+            options={
+                'ordering': ['-creation_timestamp'],
+            },
+        ),
+    ]
