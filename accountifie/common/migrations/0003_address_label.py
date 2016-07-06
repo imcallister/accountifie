87ffdc729019aa3c9a8f72a3c44c7218af8b6049
imcallister <ian.mcallister@electronifie.com>
2016-07-06 00:15:26 -0400

    add address model

---
+++ accountifie/common/migrations/0003_address_label.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('common', '0002_address'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='address',
+            name='label',
+            field=models.CharField(default='Default', max_length=20),
+            preserve_default=False,
+        ),
+    ]
