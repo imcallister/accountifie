385fba13c485a2dd9f26c9d66bcf3ab894d11274
Ian McAllister <ian@mcallisternevins.com>
2016-10-13 16:38:26 -0400

    updating forecasts to use celery

--- accountifie/forecasts/urls.py
+++ accountifie/forecasts/urls.py
@@ -1,17 +1,16 @@
 from django.conf.urls import url
 
-from accountifie.tasks.views import FinishedTask
+import views
+import jobs
 
-from .views import *
 
 urlpatterns = [
-    url(r'forecasts/get_report/$', create_report),
-    url(r'forecasts/run', forecast_run, name='run_forecast'),
-    url(r'forecasts/reportpack/finish/$', FinishedTask.as_view(template_name='forecasts/forecast_finish.html'), name="fcast_finished"),
-    url(r'forecasts/upload_gl/$', upload_gl, name='upload_gl'),
-    url(r'forecasts/forecast/hardcode_projections', hardcode_projections, name="hardcode_projections"),
-    url(r'forecasts/forecast/full_projections', all_projections, name="full_projections"),
-    url(r'forecasts/reports/(?P<fcast_id>[()_a-zA-Z0-9]+)/(?P<rpt_id>[_a-zA-Z0-9]+)/$', fcast_report, name='fcast_report'),
-    url(r'forecasts/forecast/(?P<id>[()_a-zA-Z0-9]+)/$', forecast_detail, name='forecasts_detail'),
-    url(r'^forecasts/', forecast_index, name='forecasts_index'),
+    url(r'forecasts/run', jobs.run_forecast, name='run_forecast'),
+
+    url(r'forecasts/upload_gl/$', views.upload_gl, name='upload_gl'),
+    url(r'forecasts/forecast/hardcode_projections', views.hardcode_projections, name="hardcode_projections"),
+    url(r'forecasts/forecast/full_projections', views.all_projections, name="full_projections"),
+    url(r'forecasts/reports/(?P<fcast_id>[()_a-zA-Z0-9]+)/(?P<rpt_id>[_a-zA-Z0-9]+)/$', views.fcast_report, name='fcast_report'),
+    url(r'forecasts/forecast/(?P<id>[()_a-zA-Z0-9]+)/$', views.forecast_detail, name='forecasts_detail'),
+    url(r'^forecasts/index/', views.forecast_index, name='forecasts_index'),
 ]
