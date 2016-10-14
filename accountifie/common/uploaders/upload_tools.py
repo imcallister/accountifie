24efbdba2394b8ed1f698aa4808760367bd42ceb
Ian McAllister <ian@mcallisternevins.com>
2016-10-14 17:27:53 -0400

    midway on uploaders

--- accountifie/common/uploaders/upload_tools.py
+++ accountifie/common/uploaders/upload_tools.py
@@ -1,8 +1,66 @@
 import os
 import csv
 
+from django.conf import settings
+from django.contrib import messages
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
-def csv_to_modelattr(open_file, name_cleaner=None, company=gl_helpers.get_default_company()):
+import accountifie.common.uploaders
+from accountifie.toolkit.forms import FileForm, LabelledFileForm
+from accountifie.toolkit.utils import utcnow, get_default_company
+
+import logging
+logger = logging.getLogger('default')
+
+
+DATA_ROOT = getattr(settings, 'DATA_DIR', os.path.join(settings.ENVIRON_DIR, 'data'))
+INCOMING_ROOT = os.path.join(DATA_ROOT, 'incoming')
+PROCESSED_ROOT = os.path.join(DATA_ROOT, 'processed')
+
+
+def save_file(infile):
+    new_name = '%s_%s' % (utcnow().strftime('%Y%m%d%H%M%S'), infile._name)
+    if not os.path.exists(INCOMING_ROOT):
+        os.makedirs(INCOMING_ROOT)
+    full_path = os.path.join(INCOMING_ROOT, new_name)
+    with open(full_path, 'wb') as destination:
+        for chunk in infile.chunks():
+            destination.write(chunk)
+    return new_name 
+
+
+def order_upload(request, processor, redirect_url=None, label=False):
+    if label:
+        form = LabelledFileForm(request.POST, request.FILES)
+    else:
+        form = FileForm(request.POST, request.FILES)
+
+    if form.is_valid():
+        
+        upload = request.FILES.values()[0]
+        file_name_with_timestamp = save_file(upload)
+
+        if label:
+            summary_msg, error_msgs = processor(file_name_with_timestamp,
+                                                label=form.cleaned_data['label'])
+        else:
+            summary_msg, error_msgs = processor(file_name_with_timestamp)
+        
+        messages.success(request, summary_msg)
+        for err in error_msgs:
+            messages.error(request, err)
+    else:
+        msg = 'Could not process the file provided, please see below'
+        messages.error(request, msg)
+    
+    if redirect_url:
+        return HttpResponseRedirect(redirect_url)
+    else:
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+
+def csv_to_modelattr(open_file, name_cleaner=None, company=get_default_company()):
     '''takes the fields and values in the CSV and transforms them into a list of dicts where the keys
      will match model attributes. for example Full Description becomes full_description'''
     if name_cleaner == None:
@@ -51,7 +109,7 @@ def dirty_key(row, model=None, unique=None, name_cleaner=None, value_cleaner=Non
 
     return dirty
     
-def create_instance(row, model, name_cleaner=None, value_cleaner=None, unique=None, exclude=[], company=gl_helpers.get_default_company()):
+def create_instance(row, model, name_cleaner=None, value_cleaner=None, unique=None, exclude=[], company=get_default_company()):
     row['company'] = company
     non_rel_instance = instance_nonrel_data(row, model, name_cleaner=name_cleaner, value_cleaner=value_cleaner)
     if non_rel_instance.id in exclude:
