358ebb7f502141302544e92fb4b6df948c3e694a
Ian McAllister <ian@mcallisternevins.com>
2017-11-10 21:09:08 -0500

    to v0.2

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ from setuptools import setup, find_packages
 setup(name='accountifie',
       description='Core Accouting Functionality',
       author='Ian McAllister',
+      version = '0.2',
       author_email='ian.mcallister@electronifie.com',
       url='https://github.com/electronifie/accountifie',
       packages=find_packages(),
       include_package_data=True,
      )
-
