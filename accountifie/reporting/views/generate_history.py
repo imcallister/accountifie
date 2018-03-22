7564c13eedd8058726e84168860096339e949a11
Ian McAllister <ian@mcallisternevins.com>
2018-03-22 18:45:52 -0400

    checkpoint

--- accountifie/reporting/views/generate_history.py
+++ accountifie/reporting/views/generate_history.py
@@ -1,12 +1,10 @@
 from decimal import Decimal
-import csv
 
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
 from django.shortcuts import render
 from django.template import RequestContext, Context
 from django.core.serializers.json import DjangoJSONEncoder
-from django.http import HttpResponse
 
 from accountifie.cal.models import Year
 from accountifie.common.api import api_func
@@ -48,7 +46,6 @@ def create_row(row, col_order, qs=None, fmtr=ACCT_DEF):
 def history(request, type, id):
     qs = request.GET.urlencode()
     config = rptutils.history_prep(request)
-    as_csv = (request.GET.get('as_csv', 'false').lower() == 'true')
 
     if type == 'account':
         acct = api_func('gl', 'account', id)
@@ -67,28 +64,17 @@ def history(request, type, id):
     
 
     hist_rslts = rptg_api.history(id, config)
-    if as_csv:
-        response = HttpResponse(content_type='text/csv')
-        response['Content-Disposition'] = 'attachment; filename="report.csv"'
-
-        writer = csv.writer(response)
-        writer.writerow(column_titles)
-        for h in hist_rslts:
-            writer.writerow([h[col] for col in column_titles])
-        return response
-
+    if hist_rslts:
+        entries = [create_row(h, column_titles, qs=qs) for h in hist_rslts]
     else:
-        if hist_rslts:
-            entries = [create_row(h, column_titles, qs=qs) for h in hist_rslts]
-        else:
-            entries = []
-        
-        context = {}
-        context['display_name'] = display_name
-        context['column_titles'] = column_titles
-        context['history'] = entries
-        context['years'] = Year.objects.all()
-        context['by_date_cleared'] = False
-        context['from_date'] = config.get('from', settings.DATE_EARLY)
-        context['to_date'] = config.get('to', settings.DATE_LATE)
-        return render(request, 'history.html', context)
+        entries = []
+    
+    context = {}
+    context['display_name'] = display_name
+    context['column_titles'] = column_titles
+    context['history'] = entries
+    context['years'] = Year.objects.all()
+    context['by_date_cleared'] = False
+    context['from_date'] = config.get('from', settings.DATE_EARLY)
+    context['to_date'] = config.get('to', settings.DATE_LATE)
+    return render(request, 'history.html', context)
