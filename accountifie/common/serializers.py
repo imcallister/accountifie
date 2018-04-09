7ed2231765d65baaa4588fccb56d59043d9d75fd
Ian McAllister <ian@mcallisternevins.com>
2018-04-08 23:48:57 -0400

    add log id to serializer

--- accountifie/common/serializers.py
+++ accountifie/common/serializers.py
@@ -29,4 +29,4 @@ class AddressSerializer(serializers.ModelSerializer):
 class LogSerializer(serializers.ModelSerializer):
     class Meta:
         model = Log
-        fields = ('level', 'time', 'message', 'corrId')
+        fields = ('id', 'level', 'time', 'message', 'corrId')
